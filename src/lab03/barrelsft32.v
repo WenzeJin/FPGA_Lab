f430c912974ede064ce117bd3cbbed4a8e274518
WenzeJin <wenzejin2004@gmail.com>
2023-10-07 00:24:15 +0800

    adder32

--- src/lab03/barrelsft32.v
+++ src/lab03/barrelsft32.v
@@ -21,13 +21,13 @@
 
 
 module barrelsft32(
-      output [31:0] dout,
-      input [31:0] din,
-      input [4:0] shamt,     //移动位数
-      input LR,           // LR=1时左移，LR=0时右移
-      input AL            // AL=1时算术右移，AR=0时逻辑右移
+    output [31:0] dout,
+    input [31:0] din,
+    input [4:0] shamt,     //移动位数
+    input LR,           // LR=1时左移，LR=0时右移
+    input AL            // AL=1时算术右移，AR=0时逻辑右移
 	);
-//add your code here
+
 
 
 
